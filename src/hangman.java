c84afc4e337c439d7be7ed1045dc94632ad6b25a
Gabriel Eduardo <gabrielsegurao@me.com>
2021-10-21 00:14:58 -0500

    Working mini project 1

---
+++ src/hangman.java
@@ -0,0 +1,135 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.*;
+
+public class hangman {
+
+    public static void main(String[] args) {
+
+        try {
+            Scanner txt = new Scanner(new File("src/cities.txt"));
+
+            // Create an empty list and store each line of the file in the new list
+            ArrayList<String> list = new ArrayList<>();
+            while (txt.hasNextLine()) {
+                list.add(txt.nextLine());
+            }
+
+            // Select a random word
+            Random word = new Random();
+            String random_word = list.get(word.nextInt(list.size()));
+
+            // To store the characters that the player guesses
+            ArrayList<Character> player_guesses = new ArrayList<>();
+
+            // Convert the word to char array
+            char[] char_word = random_word.toCharArray();
+
+            // Creating arrays
+            int[] number_charword = new int[char_word.length]; // the same array than 'charword' but in numbers
+
+            int wrong = 0; // variable that counts wrong letters
+            System.out.println(random_word);
+            System.out.println("Here's the question.");
+
+            // printing the hidden word "________"
+            for  (int i = 0; i < random_word.length(); i++) {
+                if (player_guesses.contains(random_word.charAt(i))){
+                    System.out.print(random_word.charAt(i));
+                }
+                else{
+                    System.out.print("_");
+                }
+            }
+
+            // elements for the scanner
+            String letter;
+            Scanner keyboard = new Scanner(System.in);
+            boolean right = false;
+
+            int guesses = 1;
+            int wrong_ch = guesses;
+            int display = 0;
+            while(true) {
+
+                if (display == 0) {
+                    System.out.println();
+                    System.out.print("Guess a letter: ");
+                    letter = keyboard.nextLine();
+                    display++;
+                } else{
+                    System.out.print("You are guessing: ");
+                    // Printing the array with the correct letters
+                    for (int i = 0; i < char_word.length; i++) {
+                        if (number_charword[i] == 1) {
+                            System.out.print(char_word[i]);
+                        }
+                        if (number_charword[i] == 3) {
+                            System.out.print(" ");
+                        }
+                        if (number_charword[i] == 0) {
+                            System.out.print("_");
+                        }
+                    }
+                    System.out.println();
+                    System.out.print("Guess a letter: ");
+                    letter = keyboard.nextLine();
+                }
+
+                char char_letter = letter.charAt(0); // Convert the String letter into char to compare
+
+                // Check if the letter which was typing is in the hidden word
+                for (int i = 0; i < char_word.length; i++) {
+                    if (char_letter == char_word[i]) {
+                        number_charword[i] = 1;
+                        right = true;
+                    }
+                }
+
+                // Player will lose the game after 10 errors with no success
+                if(guesses == 10){
+                    System.out.println("You have guessed (" + guesses + ") wrong letters: " );
+                    System.out.println("You lose!" );
+                    System.out.println("The correct word was '" + random_word + "'" );
+                    break;
+                }
+
+                // Check if the hidden word is totally revealed
+                int check_word = 0;
+                for (int i = 0; i < number_charword.length; i++) {
+                    if (number_charword[i] == 1 || number_charword[i] == 3) {
+                        check_word = check_word + 1;
+                    }
+                }
+
+                if (check_word == number_charword.length) {
+                    System.out.println();
+                    System.out.println("You win!");
+                    System.out.println("You have guessed '" + random_word + "' correctly!");
+                    break;
+                }
+
+                // If a character is wrong
+                // still trying to resolve to print after a correct character is given.
+                if (!right && wrong_ch == guesses){
+                    System.out.print("You have guessed (" + guesses + ") wrong letters: " + char_letter);
+                    System.out.println();
+                }
+
+                guesses++;
+
+            }
+
+
+        }
+        catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("File not found!");
+        }
+
+
+    }
+
+
+}
